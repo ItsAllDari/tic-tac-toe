aaaf2c4b34e1ee6b8b9913918c4bc2fc0bb6cdac
ItsAllDari <itsalldari@gmail.com>
2020-06-08 11:08:53 -0400

    Updated App Javascript
    Added the authorization events for sign in, up and out

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,9 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
-
+const authEvents = require('./auth/events')
 $(() => {
-  // your JS code goes here
+  $('#sign-up').on('submit', authEvents.onSignUp)
+  $('#sign-in').on('submit', authEvents.onSignIn)
+  $('#sign-out').on('submit', authEvents.onSignOut)
 })
