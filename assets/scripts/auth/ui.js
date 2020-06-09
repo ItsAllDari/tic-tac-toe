9857f11b8cbd9d6db78155a58db9d486a742514d
ItsAllDari <itsalldari@gmail.com>
2020-06-09 09:51:40 -0400

    Updated UI file
    Added the proper store tags for sign out message

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,49 @@
+'use strict'
+const store = require('./../store')
+
+const newSuccess = function (data) {
+  $('form').trigger('reset')
+  $('#message').text('Sign up was successful!')
+  $('#message').show().removeClass().addClass('success')
+  console.log('newSuccess')
+}
+const newFailure = function () {
+  $('form').trigger('reset')
+  $('#message').text('Sign up failed!')
+  $('#message').show().removeClass().addClass('failure')
+  console.log('newFailure')
+}
+
+const signInSuccess = function (data) {
+  $('form').trigger('reset')
+  $('#message').text("You've signed in!")
+  $('#message').show().removeClass().addClass('success')
+  store.user = data.user
+}
+const signInFailure = function () {
+  $('form').trigger('reset')
+  $('#message').text('Sign in failed')
+  $('#message').show().removeClass().addClass('failed')
+}
+
+const signOutSuccess = function () {
+  $('form').trigger('reset')
+  $('#message').text("You've signed out!")
+  $('#message').show().removeClass().addClass('success')
+  console.log('Sign Out Successful!')
+  store.user = null
+}
+const signOutFailure = function () {
+  $('form').trigger('reset')
+  $('#message').text('Sign out failed')
+  $('#message').show().removeClass().addClass('failed')
+}
+
+module.exports = {
+  newSuccess: newSuccess,
+  newFailure: newFailure,
+  signInSuccess: signInSuccess,
+  signInFailure: signInFailure,
+  signOutSuccess: signOutSuccess,
+  signOutFailure: signOutFailure
+}
