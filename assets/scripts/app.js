297d31f7048f504b8a4c9bb3fdc184e3980d1637
ItsAllDari <itsalldari@gmail.com>
2020-06-11 11:02:12 -0400

    Fixed the set up for New Game button
    Changed the route from authEvents to gameEvents

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,8 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 const authEvents = require('./auth/events')
+const gameEvents = require('./game/events-game')
+
 $(() => {
   $('#sign-up').on('submit', authEvents.onSignUp)
   $('#sign-in').on('submit', authEvents.onSignIn)
@@ -14,5 +16,5 @@ $(() => {
   $('.hide').hide()
   $('#new-game').hide()
   $('.box').hide()
-  $('#new-game').on('submit', authEvents.onNewGame)
+  $('#new-game').on('submit', gameEvents.onNewGame)
 })
